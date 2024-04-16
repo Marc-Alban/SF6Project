8ae84ab41fc19ad797966b86867d67fb402c6a60
Marc-Alban <millet.marcalban@gmail.com>
2024-04-16 22:31:42 +0200

    Add dd for s6 with request

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/{firstname}/{lastname}', name: 'app')]
+    public function index(Request $request ,$firstname,$lastname): Response
+    {
+        dd($request);
+        return $this->render('page.html',[
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+        ]);
+    }
+}
